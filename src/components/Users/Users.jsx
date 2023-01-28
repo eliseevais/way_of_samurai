dce24f6cd44bb0afa63cdba1d9effde7c80fc1c5
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2023-01-28 23:01:24 +0300

    lesson_51_loading

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,37 +1,44 @@
 import React from 'react';
 import styles from './Users.module.css';
+import axios from 'axios';
+import userPhoto from '../../assets/images/user.png'
 
 let Users = (props) => {
 
   if (props.users.length === 0) {
-    
 
-    props.setUsers([
-      {
-        id: 1,
-        photoUrl: 'https://avatars.mds.yandex.net/i?id=0b6108c3b803ffea84c028a45e84533c207375bc-4303535-images-thumbs&n=13',
-        followed: false,
-        fullName: 'Dmitry',
-        status: 'I am a boss',
-        location: { city: 'Minsk', country: 'Belarus' }
-      },
-      {
-        id: 2,
-        photoUrl: 'https://avatars.mds.yandex.net/i?id=0b6108c3b803ffea84c028a45e84533c207375bc-4303535-images-thumbs&n=13',
-        followed: true,
-        fullName: 'Sveta',
-        status: 'Hello everybody',
-        location: { city: 'Kiev', country: 'Ukrane' }
-      },
-      {
-        id: 3,
-        photoUrl: 'https://avatars.mds.yandex.net/i?id=0b6108c3b803ffea84c028a45e84533c207375bc-4303535-images-thumbs&n=13',
-        followed: false,
-        fullName: 'Irina',
-        status: 'Bla-bla-bla',
-        location: { city: 'Moscow', country: 'Russia' }
-      },
-    ])
+    axios
+      .get('https://social-network.samuraijs.com/api/1.0/users')
+      .then(response => {
+        props.setUsers(response.data.items)
+      });
+
+    // props.setUsers([
+    //   {
+    //     id: 1,
+    //     photoUrl: 'https://avatars.mds.yandex.net/i?id=0b6108c3b803ffea84c028a45e84533c207375bc-4303535-images-thumbs&n=13',
+    //     followed: false,
+    //     fullName: 'Dmitry',
+    //     status: 'I am a boss',
+    //     location: { city: 'Minsk', country: 'Belarus' }
+    //   },
+    //   {
+    //     id: 2,
+    //     photoUrl: 'https://avatars.mds.yandex.net/i?id=0b6108c3b803ffea84c028a45e84533c207375bc-4303535-images-thumbs&n=13',
+    //     followed: true,
+    //     fullName: 'Sveta',
+    //     status: 'Hello everybody',
+    //     location: { city: 'Kiev', country: 'Ukrane' }
+    //   },
+    //   {
+    //     id: 3,
+    //     photoUrl: 'https://avatars.mds.yandex.net/i?id=0b6108c3b803ffea84c028a45e84533c207375bc-4303535-images-thumbs&n=13',
+    //     followed: false,
+    //     fullName: 'Irina',
+    //     status: 'Bla-bla-bla',
+    //     location: { city: 'Moscow', country: 'Russia' }
+    //   },
+    // ])
   }
 
   return (
@@ -40,7 +47,7 @@ let Users = (props) => {
         props.users.map(u => <div key={u.id}>
           <span>
             <div>
-              <img src={u.photoUrl} className={styles.userPhoto} />
+              <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
             </div>
             <div>
               {u.followed
@@ -50,12 +57,12 @@ let Users = (props) => {
           </span>
           <span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.city}</div>
+              <div>{'u.location.country'}</div>
+              <div>{'u.location.city'}</div>
             </span>
           </span>
         </div>)
