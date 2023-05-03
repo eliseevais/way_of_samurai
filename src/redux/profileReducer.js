9726458d2bfd44c2e0e340b8fb2cb302159cf433
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2023-05-03 21:40:03 +0300

    loading_67_lesson

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,3 +1,5 @@
+import { usersAPI } from "../api/api";
+
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
@@ -23,21 +25,21 @@ const profileReducer = (state = initialState, action) => {
         message: state.newPostText,
         likesCount: 0
       };
-      return { 
-        ...state, 
+      return {
+        ...state,
         posts: [...state.posts, newPost],
         newPostText: '',
       };
     }
     case UPDATE_NEW_POST_TEXT: {
-      return { 
+      return {
         ...state,
         newPostText: action.newText,
       };
     }
     case SET_USER_PROFILE: {
       return {
-        ...state, 
+        ...state,
         profile: action.profile
       }
     }
@@ -48,6 +50,13 @@ const profileReducer = (state = initialState, action) => {
 
 export const addPostActionCreator = () => ({ type: ADD_POST });
 export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile });
+
+export const getUserProfile = (userId) => (dispatch) => {
+  usersAPI.getProfile(userId).then(response => {
+    dispatch(setUserProfile(response.data));
+  });
+};
+
 export const updateNewPostActionCreator = (text) =>
   ({ type: UPDATE_NEW_POST_TEXT, newText: text });
 
