505be2e6d6ce32affbaa48aec8ab712c48245650
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2023-06-03 20:34:03 +0300

    loading_79_lesson

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -23,7 +23,7 @@ class ProfileContainer extends React.Component {
   componentDidMount() {
     let userId = this.props.router.params.userId;
     if (!userId) {
-      userId = 1049 //28730
+      userId = this.props.authorisedUserId;
     }
     this.props.getUserProfile(userId);
     this.props.getStatus(userId);
@@ -31,15 +31,19 @@ class ProfileContainer extends React.Component {
 
   render() {
     return (
-      <Profile {...this.props} profile={this.props.profile}
-        status={this.props.status} updateStatus={this.props.updateStatus} />
+      <Profile {...this.props}
+        profile={this.props.profile}
+        status={this.props.status}
+        updateStatus={this.props.updateStatus} />
     )
   }
 }
 
 let mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
-  status: state.profilePage.status
+  status: state.profilePage.status,
+  authorisedUserId: state.auth.userId,
+  isAuth: state.auth.isAuth
 });
 
 export default compose(
