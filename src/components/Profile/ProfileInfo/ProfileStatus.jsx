2dbec99c92278fc43391b4bc82b465a607f97bb0
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2023-06-07 21:48:22 +0300

    loading_84_lesson

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -41,7 +41,8 @@ class ProfileStatus extends React.Component {
       <div>
         {!this.state.editMode &&
           <div>
-            <span onDoubleClick={this.activateEditMode}>{this.props.status || '-----'}</span>
+            <span onDoubleClick={this.activateEditMode}>
+              {this.props.status || '-----'}</span>
           </div>
         }
         {this.state.editMode &&
