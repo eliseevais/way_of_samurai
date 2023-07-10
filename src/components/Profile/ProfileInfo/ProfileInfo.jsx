c9db59f940582d8ba6216332c52c30f4c53a4a39
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2023-07-10 22:50:48 +0300

    lesson_97 part_1 loading

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Preloader from "../../Common/Preloader/Preloader";
-import s from './ProfileInfo.module.css';
+import style from './ProfileInfo.module.css';
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
-import userPhoto from '../../../assets/images/user.png'; 
+import userPhoto from '../../../assets/images/user.png';
+import ProfileDataForm from "./ProfileDataForm";
 
 const ProfileInfo = (props) => {
+
+  let [editMode, setEditMode] = useState(false);
+
   if (!props.profile) {
     return <Preloader />
   }
@@ -17,29 +21,50 @@ const ProfileInfo = (props) => {
 
   return (
     <div>
-      <div className={s.descriptionBlock}>
-        <img src={props.profile.photos.large || userPhoto} className={s.mainPhoto}/>
+      <div className={style.descriptionBlock}>
+        <img src={props.profile.photos.large || userPhoto} className={style.mainPhoto} />
         {props.isOwner && <input type={"file"} onChange={onMainPhotoSelected} />}
-        <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
 
-        <div>Обо мне: {props.profile.aboutMe}</div>
-        <div>Мои контакты</div>
-        <div>{props.profile.contacts.facebook}</div>
-        <div>{props.profile.contacts.website}</div>
-        <div>{props.profile.contacts.vk}</div>
-        <div>{props.profile.contacts.twitter}</div>
-        <div>{props.profile.contacts.instagram}</div>
-        <div>{props.profile.contacts.youtube}</div>
-        <div>{props.profile.contacts.github}</div>
-        <div>{props.profile.contacts.mainLink}</div>
-        <div>Статус поиска работы:</div>
-        <div>{props.profile.lookingForAJob}</div>
-        <div>{props.profile.lookingForAJobDescription}</div>
-        <div>Полное имя: {props.profile.fullName}</div>
-        ava + description
+        <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
+        {editMode
+          ? <ProfileDataForm profile={props.profile} />
+          : <ProfileData goToEditMode={() => { setEditMode(true) }}
+            profile={props.profile} isOwner={props.isOwner} />
+        }
       </div>
     </div>
   )
+}
+
+const ProfileData = ({ profile, isOwner, goToEditMode }) => {
+  return <div>
+    {isOwner && <div><button onClick={goToEditMode}>edit</button></div>}
+    <div>
+      <b>About me</b>: {profile.aboutme}
+    </div>
+    <div>
+      <b>Full name</b>: {profile.fullName}
+    </div>
+    <div>
+      <b>Looking for a job</b>: {profile.lookingForAJob ? "yes" : "no"}
+    </div>
+    {profile.lookingForAJob &&
+      <div>
+        <b>My professional skills</b>: {profile.lookingForAJobDescription}
+      </div>
+    }
+
+    <div>
+      <b>Contacts</b>: {Object.keys(profile.contacts).map(key => {
+        return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]} />
+      })
+      }
+    </div>
+  </div>
+}
+
+const Contact = ({ contactTitle, contactValue }) => {
+  return <div className={style.contact}><b>{contactTitle}</b>: {contactValue}</div>
 }
 
 export default ProfileInfo;
